39d0e98944ecc8fe61747ae5d4b8d26940dabe91
Nignik <maskymilian.olbrys@gmail.com>
2024-10-27 01:00:13 +0200

    init

---
+++ HoriInvaders/src/HoriInvaders.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+#include <HoriEngine.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	Hori::Engine engine;
+
+	engine.Run();
+
+	return 0;
+}
